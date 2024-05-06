5320c7877b5eaaecf33bb8a285407bbc39200516
nikhilmaurya728 <126176483+nikhilmaurya728@users.noreply.github.com>
2024-05-06 10:33:42 +0530

    u

--- pyproject1/settings.py
+++ pyproject1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v=_&agbmoz-)vxsyamrubm&%rfs7i+ne+_e+q$0u9$np18nqfd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh' ,  '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh' ,  '127.0.0.1', 'localhost']
 
 
 # Application definition
