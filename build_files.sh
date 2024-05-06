f54cf730a6c6e9689f5cfa838369b5ab9e7145f4
nikhilmaurya728 <126176483+nikhilmaurya728@users.noreply.github.com>
2024-05-06 15:24:35 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo BUILD START
+python  -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo BUILD END
